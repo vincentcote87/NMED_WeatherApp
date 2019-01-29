dee06c83a853ece77f762c021a8aaad3a122b32a
Vincent <vincentcote87@gmail.com>
2019-01-29 12:21:41 -0700

    Added other weather perameters and tweaked css

--- WeatherApp/platforms/browser/www/js/weatherapp.js
+++ WeatherApp/platforms/browser/www/js/weatherapp.js
@@ -7,7 +7,7 @@ window.onload = function () {
 };
 
 function init() {  
-    var location = "lethbridge,ca";
+    var location = "Lethbridge,ca";
     weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=metric&APPID=ac4442d3cc94f73f2a14aabd2a07da36";
     weather();
 }
@@ -25,12 +25,12 @@ function getWeather() { // when readystate changes
     if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
 
         var json = JSON.parse(xmlhttp.responseText);
-
+        setCurrentIcon(json.weather[0].id);
         function Forcast(Temp, Descrip, Wind, Max, Min, Humid) {          
             document.getElementById("temp").innerHTML = Math.round(Temp);
             document.getElementById("description").innerHTML = Descrip;
-            document.getElementById("mintemp").innerHTML = Min;
-            document.getElementById("maxtemp").innerHTML = Max;
+            document.getElementById("mintemp").innerHTML = Min; //Not sure Min and Max work for Lethbridge
+            document.getElementById("maxtemp").innerHTML = Max; // Seems to return the same as current temp
             document.getElementById("wind").innerHTML = Wind;
             document.getElementById("humidity").innerHTML = Humid;
         }
@@ -42,4 +42,24 @@ function getWeather() { // when readystate changes
     } else {
         console.log("no dice");
     }
+}
+
+function setCurrentIcon(code) {
+    let className;
+    switch (String(code)[0]) {
+        case "2": className = "wi wi-day-thunderstorm curTempIcon"; break;
+        case "3": className = "wi wi-day-sprinkle curTempIcon"; break;
+        case "5": className = "wi wi-day-rain curTempIcon"; break;
+        case "6": className = "wi wi-day-snow"; break;
+        case "7": className = "wi wi-day-fog"; break;
+        case "8": 
+            if (code == 800) { 
+                className = "wi wi-day-sunny";
+            } else {
+                className = "wi wi-day-cloudy";
+            }; break;
+        default: className = "wi wi-day-cloudy";
+    }
+
+    document.getElementById("curTempIcon").className = className;
 }
